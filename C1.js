9317e25842b7d912fa4a2cac48f370404e7c289b
AliMourad250 <thexali123@gmail.com>
2023-08-30 00:54:41 +0300

    Challenge 1 - Done with Step 3

--- C1.js
+++ C1.js
@@ -1,3 +1,12 @@
 console.log('I am linked now!!');
-let name = "Ali";
+let name;
+name = "Ali";
 alert(name);
+
+let age = 21;
+var lastName = "Mrad";
+const DOB = "07/07/2002";
+
+
+
+console.log(age + ", " + lastName + ", " + DOB);
