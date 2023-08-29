038191627ee1a80fa206cf81c7af00289521bf0d
AliMourad250 <thexali123@gmail.com>
2023-08-30 01:06:35 +0300

    Challenge 1 - Done with Step 4

--- C1.js
+++ C1.js
@@ -10,3 +10,29 @@ const DOB = "07/07/2002";
 
 
 console.log(age + ", " + lastName + ", " + DOB);
+console.log(typeof ("this is a text"));
+console.log(typeof (true));
+console.log(typeof (false));
+console.log(typeof (1215));
+console.log(typeof ("999"));
+console.log(typeof ({}));
+console.log(typeof ([]));
+console.log(typeof (null));
+console.log(typeof (undefined));
+
+let v1 = "what time is it?",
+    v2 = true,
+    v3 = 8,
+    v4 = [21, 22, 23, 2222],
+    v5 = {
+        "food": "burger",
+        "drink": ["water", "juice"]
+    },
+    v6 = 'c';
+
+console.log(v1 + ": " + typeof (v1));
+console.log(v2 + ": " + typeof (v2));
+console.log(v3 + ": " + typeof (v3));
+console.log(v4 + ": " + typeof (v4));
+console.log(v5 + ": " + typeof (v5));
+console.log(v6 + ": " + typeof (v6));
