fb340a427f4fc878ab75569e4615954fd829c7af
AliMourad250 <thexali123@gmail.com>
2023-09-01 14:17:55 +0300

    Challenge 4 - Done with step 2

--- C4.js
+++ C4.js
@@ -5,4 +5,11 @@ let personalInfo = {
     height: '178 cm',
     location: 'Shouf',
     nationality: 'Lebanese',
-}
+};
+
+let nextThreeNumbers = [6, 7, 8];
+let allNumbers = [...fiveNumbers, ...nextThreeNumbers];
+console.log(allNumbers);
+
+console.log("----------------");
+
