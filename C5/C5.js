73773f834bea7ab01f96500734f083ee3003cf13
AliMourad250 <thexali123@gmail.com>
2023-09-03 14:20:33 +0300

    Challenge 5.1 - Done with step 2

--- C5/C5.js
+++ C5/C5.js
@@ -20,3 +20,46 @@ button.addEventListener('click', () => {
 
 // ---------------------------------------------
 
+let header1 = document.createElement('h1');
+header1.innerText = "Hello World!";
+header1.style.border = "10px dotted green"
+
+document.body.appendChild(header1);
+document.addEventListener('keydown', (event) => {
+    console.log("Key pressed:", event.key);
+    switch (event.key) {
+        case 'A':
+        case 'a':
+            header1.style.backgroundColor = "red";
+            break;
+
+        case 'S':
+        case 's':
+            header1.style.marginLeft = "10px";
+            break;
+
+        case 'D':
+        case 'd':
+            let par = document.createElement('p');
+            par.innerText = `Key D was pressed!`;
+            document.body.appendChild(par);
+            break;
+
+        case 'W':
+        case 'w':
+            if (header1.style.visibility = "visible") {
+                header1.style.visibility = "hidden";
+            }
+            else if (header1.style.visibility = "hidden") {
+                header1.style.visibility = "visible";
+            }
+            break;
+
+        case ' ':
+            header1.style.fontSize="80px";
+            break;
+    }
+})
+
+// ---------------------------------------------
+
