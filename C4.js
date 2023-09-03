199f680c18d533b3c0ab31e3eb1bffc9325a948e
AliMourad250 <thexali123@gmail.com>
2023-09-03 13:08:13 +0300

    Challenge 4 - Done with step 4

--- C4.js
+++ C4.js
@@ -5,6 +5,10 @@ let personalInfo = {
     height: '178 cm',
     location: 'Shouf',
     nationality: 'Lebanese',
+    contactInfo: {
+        email: 'myEmail@gmail.com',
+        phoneNo: '71559162',
+    }
 };
 
 let nextThreeNumbers = [6, 7, 8];
@@ -22,3 +26,27 @@ console.log(name + " " + location + " " + nationality);
 
 console.log("----------------");
 
+let { email, phoneNo } = personalInfo.contactInfo;
+console.log(email + " " + phoneNo);
+
+let [first, second, ...restOfNumbers] = fiveNumbers;
+console.log("first= " + first);
+console.log("second= " + second);
+console.log("restOfNumbers= " + restOfNumbers);
+
+const PersInf = {
+    name: "ali",
+    phoneNumber: "1111",
+    age: 21
+}
+
+const changes = {
+    name: "Ali Morad",
+    phoneNumber: "71******"
+}
+
+const changedPersInf = { ...PersInf, ...changes };
+
+console.log(changedPersInf);
+
+console.log("----------------");
