51db03d44a2eb1b8c0f4d8c12a6ab07ced3fa222
AliMourad250 <thexali123@gmail.com>
2023-09-03 21:13:56 +0300

    Challenge 5 - Done with step 2

--- C5/C5.js
+++ C5/C5.js
@@ -40,3 +40,54 @@ let h1s = document.querySelectorAll('h1');
 for (let h1 of h1s) {
     h1.style.fontStyle = "italic";
 }
+
+// ---------------------------------------------
+
+let hoveredButton = document.createElement('button');
+hoveredButton.innerHTML = "Hover over me"
+hoveredButton.addEventListener('mouseover', () => {
+    hoveredButton.style.color = "red";
+});
+
+hoveredButton.addEventListener('mouseout', () => {
+    hoveredButton.style.color = "black";
+});
+
+document.body.appendChild(hoveredButton);
+
+let buttonContainer = document.getElementById('buttonContainer');
+for (let btn of buttonContainer.children) {
+    btn.addEventListener('click', () => {
+        console.log(btn.innerHTML);
+    })
+}
+
+let form = document.createElement('form');
+
+let nameField = document.createElement('input');
+nameField.type = 'text';
+nameField.placeholder = "Name here";
+
+let emailField = document.createElement('input');
+emailField.type = "email";
+
+emailField.placeholder = "Email here";
+
+let submit = document.createElement('button');
+submit.innerText = 'Submit';
+
+submit.addEventListener('click', (event) => {
+    event.preventDefault();
+    console.log(`Name: ${nameField.value}\nEmail: ${emailField.value}`)
+})
+
+form.appendChild(nameField);
+form.appendChild(emailField);
+form.appendChild(submit);
+
+document.body.appendChild(form);
+
+// ---------------------------------------------
+
+
+
